9b28769b263247aa114c87cb4ea504a65c7fc195
Mert Bozan <mbozan@c3r6p5.42vienna.com>
2024-06-23 15:02:39 +0200

    files created and starting points check

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbozan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/23 14:58:35 by mbozan            #+#    #+#             */
+/*   Updated: 2024/06/23 15:02:06 by mbozan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 420
+# endif
+
+char	*get_next_line(int fd);
